0334325c433913cb405f1c09bf4746d1348ca742
Chris Han <christopher.t.han@gmail.com>
2018-05-05 18:18:10 -0700

    generalize convert_datetime
    Converts arbitrary timezones from one to another

--- utils/dates.py
+++ utils/dates.py
@@ -1,16 +1,19 @@
-from dateutil import parser
-from dateutil import tz
+from dateutil import parser, tz
 
-def convert_datetime(date_str, target_format='%Y-%m-%d', target_tz=None):
+def convert_datetime(date_str, from_tz='UTC', target_format=None, target_tz=None):
     """
     Converts an ISO8601 formatted string to a target format and optional target timezone.
     """
 
     d = parser.isoparse(date_str)
+
     if target_tz:
-        utc = d.replace(tzinfo=tz.gettz('UTC'))
+        utc = d.replace(tzinfo=tz.gettz(from_tz))
         d = utc.astimezone(tz.gettz(target_tz))
 
-    converted = d.strftime(target_format)
+    if target_format:
+        converted = d.strftime(target_format)
+    else:
+        converted = d.isoformat()
 
     return converted
