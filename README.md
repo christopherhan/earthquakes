e4ed5077c41ea047f6261968808eefd8e93644b3
Chris Han <christopher.t.han@gmail.com>
2018-05-05 18:18:34 -0700

    Add unit tests for convert_timzone

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Clone the repo:
     $ git clone git@gitlab.com:christopherhan/earthquakes.git
     $ cd earthquakes
 
+Ensure your PYTHONPATH contains the project:
+
+    export PYTHONPATH=$PYTHONPATH:`pwd`
+
 Install `requirements.txt` and ensure you are running Python >= 3.6:
 
     $ pip install -r requirements.txt
@@ -17,6 +21,25 @@ Install `requirements.txt` and ensure you are running Python >= 3.6:
     $ python --version
     Python 3.6.5
 
-Execute `earthquake.py` (ensure you're in the `earthquakes/` directory):
+Execute `main.py` (ensure you're in the project root):
 
-    $ python earthquake.py
+    $ python main.py
+
+
+## Unit Tests
+
+To run unit tests, run:
+
+    $ python -m unittest tests/tests.py
+
+
+You should see output similar to the following:
+
+```
+(earthquakes) Chriss-MacBook-Pro:earthquakes chris$ python tests/tests.py
+.......
+----------------------------------------------------------------------
+Ran 7 tests in 0.003s
+
+OK
+```
---
+++ tests/timezone_conversion.py
@@ -0,0 +1,74 @@
+import unittest
+from utils.dates import convert_datetime
+
+class TestTimezoneConversion(unittest.TestCase):
+    def setUp(self):
+        self.utc = '2018-05-04T16:51:29.230Z'
+        self.utc_morning = '2018-05-04T09:37:36.260Z'
+        self.pyutc = '2018-05-04T16:51:29.230000+00:00'
+        self.pst = '2018-05-04T09:51:29.230000-07:00'
+        self.pst_formatted = '2018-05-04'
+        self.est = '2018-05-04T12:51:29.230000-04:00'
+        self.next_day = '2018-05-05'
+        self.previous_day = '2018-05-04'
+
+    def test_utc_to_pyutc(self):
+        """Test the ISO format provided by the dataset and convert it to
+        Python's representation.
+        """
+
+        new_date = convert_datetime(self.utc,
+                                    target_tz='UTC')
+
+        self.assertEqual(new_date, self.pyutc)
+
+    def test_utc_to_pst(self):
+        """Test UTC conversion to PST"""
+
+        new_date = convert_datetime(self.utc,
+                                    target_tz='America/Los_Angeles')
+
+        self.assertEqual(new_date, self.pst)
+
+    def test_target_format(self):
+        """Test converstion of UTC to PST in the target format"""
+
+        new_date = convert_datetime(self.utc,
+                                    target_format='%Y-%m-%d',
+                                    target_tz='America/Los_Angeles')
+
+        self.assertEqual(new_date, self.pst_formatted)
+
+    def test_utc_next_day(self):
+        """Test conversion of UTC to a timezone in the next day"""
+
+        new_date = convert_datetime(self.utc,
+                                    target_format='%Y-%m-%d',
+                                    target_tz='Asia/Macau')
+        self.assertEqual(new_date, self.next_day)
+
+    def test_utc_previous_day(self):
+        """Test conversion of UTC to a timezone in the previous day"""
+
+        new_date = convert_datetime(self.utc_morning,
+                                    target_format='%Y-%m-%d',
+                                    target_tz='America/Los_Angeles')
+
+    def test_pst_to_utc(self):
+        """Test PST conversion to UTC"""
+
+        new_date = convert_datetime(self.pst,
+                                    from_tz='America/Los_Angeles',
+                                    target_tz='UTC')
+        self.assertEqual(new_date, self.pyutc)
+
+    def test_pst_to_est(self):
+        """Test PST conversion to EST"""
+
+        new_date = convert_datetime(self.pst,
+                                    from_tz='America/Los_Angeles',
+                                    target_tz='America/New_York')
+        self.assertEqual(new_date, self.est)
+
+if __name__ == '__main__':
+    unittest.main()
